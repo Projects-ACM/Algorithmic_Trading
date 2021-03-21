e461e1b2011d305df406a165c30eee776b3e421f
jzhang1043 <jiahao4@illinois.edu>
2021-03-21 10:25:16 -0500

    tensorflow installed

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-A = 1
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import pandas as pd
+import seaborn as sns
+from pylab import rcParams
+import matplotlib.pyplot as plt
+from matplotlib import rc
+
